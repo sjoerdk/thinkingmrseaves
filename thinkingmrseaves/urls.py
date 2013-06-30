4818eefe55233fe29fc27d1b957106774661fd67
Sjoerd <sjoerdk@home.nl>
2013-06-30 18:24:45 +0200

    intial empy django project

---
+++ thinkingmrseaves/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'thinkingmrseaves.views.home', name='home'),
+    # url(r'^thinkingmrseaves/', include('thinkingmrseaves.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)
